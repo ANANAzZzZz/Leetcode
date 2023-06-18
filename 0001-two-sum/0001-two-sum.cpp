2f8eb86cb2ee465f705f70cff7b31208b7d03c49
Vlad Syvorotnev <62443900+ANANAzZzZz@users.noreply.github.com>
2023-06-18 14:16:19 +0300

    Time: 785 ms (5.43%), Space: 10 MB (94.16%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i = 0;
+        int j = 1;
+        vector<int> answer;
+
+        while (i < j) {
+            if ((nums[i] + nums[j]) == target) {
+                answer.push_back(i);
+                answer.push_back(j);
+
+            return answer;
+            } else {
+                j++;
+            }
+            if (j == nums.size()) {
+                i++;
+                j = i + 1;
+             }
+         }
+         return answer;
+    }
+};
