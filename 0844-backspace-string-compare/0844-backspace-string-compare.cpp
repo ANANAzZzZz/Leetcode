ed8cf3958e88bba14957596bb16524cbe2c64d3a
Vlad Syvorotnev <62443900+ANANAzZzZz@users.noreply.github.com>
2023-06-23 15:17:53 +0300

    Time: 0 ms (100.00%), Space: 6.4 MB (21.90%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+      stack<char> stS;
+      stack<char> stT;
+
+      for (int i = 0; i < s.size(); i++) {
+        if (s[i] != '#') {
+          stS.emplace(s[i]);
+        } else {
+          if (!stS.empty()) {
+            stS.pop();
+          }
+        }
+      }
+
+      for (int i = 0; i < t.size(); i++) {
+        if (t[i] != '#') {
+          stT.emplace(t[i]);
+        } else {
+          if (!stT.empty()) {
+            stT.pop();
+          }
+        }
+      }
+
+      if (stS == stT) {
+        return true;
+      }
+      return false;
+    }
+};
