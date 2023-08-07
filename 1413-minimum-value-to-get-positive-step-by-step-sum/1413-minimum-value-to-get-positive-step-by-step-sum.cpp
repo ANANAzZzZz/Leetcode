8f1e4cd4fbbcbd1afd532431737884926fd4a961
Vlad Syvorotnev <62443900+ANANAzZzZz@users.noreply.github.com>
2023-08-07 16:27:50 +0300

    Time: 0 ms (100.00%), Space: 7.4 MB (14.43%) - LeetHub

---
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        vector<int> prefix = {nums[0]};
+        int startValue = 1;
+        int minValue = prefix[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            prefix.push_back(prefix.back() + nums[i]);
+            minValue = min(minValue, prefix.back());
+        }
+
+        if (minValue < 0) {
+            return abs(minValue) + 1;
+        } else {
+            return 1;
+        }
+    }
+};
+
+// Time complexity - O(n)
+// Space complexity - O(n)
