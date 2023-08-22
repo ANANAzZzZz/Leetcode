9a87d0700f59c83d92182271e415c220fe459d36
Vlad Syvorotnev <62443900+ANANAzZzZz@users.noreply.github.com>
2023-08-22 22:50:20 +0300

    Time: 0 ms (100.00%), Space: 6.7 MB (36.86%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -4,19 +4,8 @@ public:
         unordered_map<char, int> count;
 
         for (int i = 0; i < text.size(); i++) {
-            if (text[i] == 'b' || text[i] == 'a' || text[i] == 'l' || text[i] == 'o' || text[i] == 'n')
-                count[text[i]]++;
+            count[text[i]]++;
         }
-
-        if (
-            !count.count('b') ||
-            !count.count('a') ||
-            !count.count('l') / 2 ||
-            !count.count('o') / 2 ||
-            !count.count('n')
-            ) {
-                return 0;
-         }
 
         return min({count['n'], count['a'], count['b'], count['l'] / 2, count['o'] / 2});
     }
