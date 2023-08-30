0552c8e30e77a9d3ce213fc68137a0168e016fa6
Vlad Syvorotnev <62443900+ANANAzZzZz@users.noreply.github.com>
2023-08-30 12:31:52 +0300

    Time: 110 ms (62.40%), Space: 71.6 MB (19.94%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> counts;
+
+        for (auto& num: nums) {
+            counts[num]++;
+        }
+
+        for (auto& num: nums) {
+            if (counts[num] >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+// Time complexity - O(n)
+// Space complexity - O(n)
